cbc189e559d7975c99eab1b92b25bc2b95e5492f
Ale V <alejandro.the.villate@gmail.com>
2024-12-25 15:35:12 -0500

    Refactor index.js
    I used GitHub copilot for the first time to help improve my code. It's pretty cool but I want to try to not rely on it too much

--- index.js
+++ index.js
@@ -6,69 +6,97 @@ const { google } = require("googleapis");
 const { authorize } = require("./auth/auth.js");
 
 const NOTE_PATH = path.join(process.cwd(), "NOTES", "test.txt");
+const FOLDER_NAME = "Test";
 
 /**
  * Get the folder ID of a GDrive folder. GDrive doesn't enforce unique
  * folder names, so if there are multiple folders with the same name,
  * gets the ID of the most recently modified by user folder by that name.
- * @param {drive_v3.Drive} drive The Google Drive API client object.
+ * @param {object} drive The Google Drive API client object.
  * @param {string} name The name of the GDrive folder being searched for
  * @returns {Promise<string>} Folder ID
  */
 async function getFolderID(drive, name) {
-  const query = `
+  try {
+    const query = `
     name='${name}' and 
     mimeType='application/vnd.google-apps.folder' and 
     trashed=false
     `;
-  const res = await drive.files.list({
-    q: query,
-    fields: "nextPageToken, files(id, name, modifiedTime)",
-    spaces: "drive",
-    orderBy: "modifiedByMeTime desc",
-  });
-  if (!res.data || !res.data.files.length) {
-    throw `No such folder: Could not locate '${name}' folder`;
-  }
+    const res = await drive.files.list({
+      q: query,
+      fields: "nextPageToken, files(id, name, modifiedTime)",
+      spaces: "drive",
+      orderBy: "modifiedByMeTime desc",
+    });
+    if (!res.data || !res.data.files.length) {
+      throw new Error(`No such folder: Could not locate '${name}' folder`);
+    }
 
-  return res.data.files[0].id;
+    return res.data.files[0].id;
+  } catch (error) {
+    console.error("Error getting folder ID:", error);
+    throw error;
+  }
+}
+
+/**
+ * Create a new Google Doc with the specified content in the specified folder.
+ * @param {string} folderId The ID of the folder where the new document will be created.
+ * @param {string} content The content to be inserted into the new document.
+ * @param {object} drive The Google Drive API client object.
+ * @param {object} docs The Google Docs API client object.
+ * @returns {Promise<void>}
+ */
+async function createGoogleDoc(folderId, content, drive, docs) {
+  try {
+    const fileMetadata = {
+      name: "Ozymandias",
+      mimeType: "application/vnd.google-apps.document",
+      parents: [folderId],
+    };
+    const file = await drive.files.create({
+      requestBody: fileMetadata,
+      fields: "id",
+    });
+    const reqBody = {
+      requests: [
+        {
+          insertText: {
+            endOfSegmentLocation: {},
+            text: content,
+          },
+        },
+      ],
+    };
+    await docs.documents.batchUpdate({
+      documentId: file.data.id,
+      requestBody: reqBody,
+    });
+  } catch (error) {
+    console.error("Error creating Google Doc:", error);
+    throw error;
+  }
 }
 
 /**
  * Gdocify
  */
 async function gdocify() {
-  const data = await fs.readFile(NOTE_PATH, "utf-8");
-  const authClient = await authorize();
+  try {
+    const data = await fs.readFile(NOTE_PATH, "utf-8");
+    const authClient = await authorize();
 
-  const drive = google.drive({ version: "v3", auth: authClient });
-  const docs = google.docs({ version: "v1", auth: authClient });
+    const drive = google.drive({ version: "v3", auth: authClient });
+    const docs = google.docs({ version: "v1", auth: authClient });
 
-  const folderId = await getFolderID(drive, "Test");
-  const fileMetadata = {
-    name: "Ozymandias",
-    mimeType: "application/vnd.google-apps.document",
-    parents: [folderId],
-  };
-  const file = await drive.files.create({
-    requestBody: fileMetadata,
-    fields: "id",
-  });
-  const reqBody = {
-    requests: [
-      {
-        insertText: {
-          endOfSegmentLocation: {},
-          text: data.toString(),
-        },
-      },
-    ],
-  };
-  docs.documents.batchUpdate({
-    documentId: file.data.id,
-    requestBody: reqBody,
-  });
+    const folderId = await getFolderID(drive, FOLDER_NAME);
+    await createGoogleDoc(folderId, data, drive, docs);
 
-  console.log("Done");
+    console.log("Done");
+  } catch (error) {
+    console.error("Error in gdocify:", error);
+  }
 }
+
 gdocify();
